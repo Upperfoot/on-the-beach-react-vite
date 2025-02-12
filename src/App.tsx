11bc8ab8aed78e54b4acff5b674412941659ce28
Craig Whiteside <upperfoot@gmail.com>
2025-02-12 15:33:22 +0000

    Add Example Data, will move this into own handler for the JSON response

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,61 @@ import { HolidayCardList } from './components/holiday-card-list/HolidayCardList'
 import { Holiday } from './types/holiday.interface';
 
 function App() {
-    const holidayA: Holiday = {
-        uuid: "test-uuid",
-        name: "Hello Hotel",
-        location: "Manchester",
-        stars: 5,
-        image: "image.jpg",
-        price: 999.99,
-        summary: "Luxurious Hotel located in the heart of Manchester"
-    };
+  const holidayA: Holiday = {
+    "resort": {
+      "id": "7dd27e42-2b5c-4237-86ac-97c26f72eb5b",
+      "name": "Iberostar Grand Salome",
+      "regionName": "Costa Adeje",
+      "countryName": "Tenerife",
+      "starRating": 5,
+      "overview": "The Iberostar Grand Salomehas an exceptional location in the south of Tenrife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Golf Courses, and is an ideal hotel for families couples and groups who are looking for a holiday full of sport, sun and sea.",
+      "image": {
+        "url": "https://static.onthebeach.co.uk/fe-code-test/hotel-image-1.jpg",
+        "description": "A tranquil resort swimming pool with clear blue water, surrounded by two-story villas with terracotta roofs under a bright blue sky."
+      }
+    },
+    "flightDetails": {
+      "departureAirport": "East Midlands",
+      "departureDate": "2030-07-03T00:00:00.000Z"
+    },
+    "bookingDetails": {
+      "party": { "adults": 2, "children": 2, "infants": 1 },
+      "lengthOfStay": 7,
+      "price": {
+        "amount": 1136.5,
+        "currency": "GBP"
+      }
+    }
+  };
 
-    const holidayB: Holiday = {
-        uuid: "test-uuid-2",
-        name: "Hello Hotel 2",
-        location: "Liverpool",
-        stars: 4,
-        image: "image.jpg",
-        price: 799.99,
-        summary: "Luxurious Hotel located in the heart of Liverpool"
-    };
+const holidayB: Holiday = {
+    "resort": {
+      "id": "f0ec3974-fb4e-4fb5-b964-2ac8a2e2c1a4",
+      "name": "Aguamarina Golf Hotel",
+      "regionName": "Costa Adeje",
+      "countryName": "Tenerife",
+      "starRating": 4,
+      "overview": "The Hotel Aguamarina Golf has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Golf courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea.",
+      "image": {
+        "url": "https://static.onthebeach.co.uk/fe-code-test/hotel-image-2.jpg",
+        "description": "A bustling resort pool complex with palm trees, white buildings with red roofing, and guests enjoying sunbeds under blue parasols."
+      }
+    },
+    "flightDetails": {
+      "departureAirport": "Liverpool",
+      "departureDate": "2030-05-27T00:00:00.000Z"
+    },
+    "bookingDetails": {
+      "party": { "adults": 2, "children": 1 },
+      "lengthOfStay": 7,
+      "price": {
+        "amount": 696.8,
+        "currency": "GBP"
+      }
+    }
+  };
 
-    const holidayList: Holiday[] = [holidayA, holidayB];
+const holidayList: Holiday[] = [holidayA, holidayB];
 
   return (
     <>
