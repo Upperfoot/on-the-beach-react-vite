05bf1862e798c290a37053bc89efd43407363183
Craig Whiteside <upperfoot@gmail.com>
2025-02-12 20:31:30 +0000

    Working Version of Holiday Service and Passing Test!

--- src/services/holiday.service.ts
+++ src/services/holiday.service.ts
@@ -1,8 +1,16 @@
+import axios from "axios";
 import { Holiday } from "../types/holiday.interface";
+
+const API_URL = "https://static.onthebeach.co.uk/fe-code-test/data.json"; // Using hardcoded value for now, could use .env files, or derive from process
 
 export class HolidayService {
     // If the API had Sort / Order Functions, I would pass this as a Parameter
     fetchAll = async (): Promise<Holiday[]> => {
-        return [];
+        try {
+            const response = await axios.get<Holiday[]>(API_URL); // I'm not manually mapping, just casting, the API may change in future however
+            return response.data;
+        } catch (error) {
+            return []; // Return empty array if fetch fails
+        }
     };
 }
