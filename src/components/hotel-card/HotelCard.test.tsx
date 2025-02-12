a2b566de4e4e7c1e8aa5a8482511c99b86f03728
Craig Whiteside <upperfoot@gmail.com>
2025-02-12 14:31:29 +0000

    Made sure we await and fix inconsistency with quotes

--- src/components/hotel-card/HotelCard.test.tsx
+++ src/components/hotel-card/HotelCard.test.tsx
@@ -4,13 +4,14 @@ import { Hotel } from "../../types/hotel.interface";
 
 test("renders HotelCard Component", async () => {
     const hotel: Hotel = {
-        uuid: 'test-uuid',
-        name: 'Hello Hotel'
+        uuid: "test-uuid",
+        name: "Hello Hotel"
     };
     
     render(<HotelCard hotel={hotel} />);
 
-    const hotelCard = screen.findByText('Hello');
+    const hotelCard = await screen.findByText("Hello");
 
-    expect(hotelCard).toBeDefined();
+    expect(hotelCard.textContent).toEqual("Hello");
+    expect(hotelCard.textContent).not.toEqual("Does Not Exist");
 });
