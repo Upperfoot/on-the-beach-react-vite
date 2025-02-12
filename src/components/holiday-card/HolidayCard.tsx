2283976cf6d563cd00bb912953b4a1178e9d129f
Craig Whiteside <upperfoot@gmail.com>
2025-02-12 16:17:41 +0000

    Lets add the data variables into the Holiday Card Component

--- src/components/holiday-card/HolidayCard.tsx
+++ src/components/holiday-card/HolidayCard.tsx
@@ -7,8 +7,21 @@ interface HolidayCardProps {
 
 export const HolidayCard: React.FC<HolidayCardProps> = ({ holiday }) => {
     return (
-      <div className={styles.holidayCard} data-testid="holiday-card">
+      <article className={styles.holidayCard} data-testid="holiday-card">
+        <div className={styles.holidayCardImage}>
+          <img src={holiday.resort.image.url} alt={holiday.resort.image.description} />
+        </div>
+        <div className={styles.holidayCardHeaderContainer}>
+          <header className={styles.holidayCardHeader}>
+            <h2>{ holiday.resort.name }</h2>
+          </header>
+          <p>{ holiday.resort.regionName}, { holiday.resort.countryName}</p>
+        </div>
         { holiday.resort.name }
-      </div>
+        <section className={styles.holidayCardDetail}>
+          <h3>Overview</h3>
+          <p>{holiday.resort.overview}</p>
+        </section>
+      </article>
     );
   };
