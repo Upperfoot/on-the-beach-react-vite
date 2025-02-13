04a303570398629ab29bc18c82f253bb2f2ce633
Craig Whiteside <upperfoot@gmail.com>
2025-02-13 14:09:40 +0000

    Mention DI for Solid Principles

--- src/services/holiday.service.ts
+++ src/services/holiday.service.ts
@@ -3,6 +3,7 @@ import { Holiday } from "../types/holiday.interface";
 
 const API_URL = "https://static.onthebeach.co.uk/fe-code-test/data.json"; // Using hardcoded value for now, could use .env files, or derive from process
 
+// I could apply Dependency Inversion here if needed by adding an interface and implementing, it's overkill for now
 export class HolidayService {
     // If the API had Sort / Order Functions, I would pass this as a Parameter
     async fetchAll(): Promise<Holiday[]> {
