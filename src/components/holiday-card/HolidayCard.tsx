6a63244c42a6d0827ccc7e36b518c43fa06667b6
Craig Whiteside <upperfoot@gmail.com>
2025-02-13 11:38:53 +0000

    Added Plurals and Comma and Ampersand Logic
    This allows us to add Plurals, and adding Commas and Ampersands in the right places

--- src/components/holiday-card/HolidayCard.tsx
+++ src/components/holiday-card/HolidayCard.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { Holiday } from "../../types/holiday.interface";
 import styles from "./HolidayCard.module.css";
+import { StarRating } from "../star-rating/StarRating";
+import { format } from "date-fns";
+import classNames from "classnames";
 
 interface HolidayCardProps {
     holiday: Holiday
@@ -8,6 +11,29 @@ interface HolidayCardProps {
 
 export const HolidayCard: React.FC<HolidayCardProps> = ({ holiday }) => {
     const [expanded, setExpanded] = useState(false);
+
+    // This allows us to iterate across the different keys of our object
+    const partyKeyTypes = (['adults', 'children', 'infants'] as Array<keyof typeof holiday.bookingDetails.party>).filter(type => holiday.bookingDetails.party[type]);
+
+    // This gets our Plural Text (Could use a Pluralisation Library like i18n)
+    const getPlural = (value: number | undefined, key: keyof typeof holiday.bookingDetails.party): string => {
+      if(value !== undefined) {
+        const moreThanOne = value > 1;
+
+        if(key === 'adults') {
+          return moreThanOne ? 'adults' : 'adult';
+        }
+
+        if(key === 'children') {
+          return moreThanOne ? 'children' : 'child';
+        }
+
+        if(key === 'infants') {
+          return moreThanOne ? 'infants' : 'infant';
+        }
+      }
+      return ''; // Empty String
+    }
 
     return (
       <article className={styles.holidayCard} data-testid="holiday-card">
@@ -28,6 +54,30 @@ export const HolidayCard: React.FC<HolidayCardProps> = ({ holiday }) => {
               <h3>{ holiday.resort.name }</h3>
             </header>
             <p>{ holiday.resort.regionName}, { holiday.resort.countryName}</p>
+            <StarRating rating={holiday.resort.starRating} />
+            <p>
+              { partyKeyTypes.map((key, index, array) => (
+                <span>
+                  {array.length > 1 && index > 0 && (index === array.length - 1 ? " & " : ", ")}
+                  <strong>{ holiday.bookingDetails.party[key] }</strong> { getPlural(holiday.bookingDetails.party[key], key) }
+                </span>
+              ))}
+              <br />
+              <span>
+                <strong>{ format(holiday.flightDetails.departureDate, "do MMM yyyy") }</strong> for <strong>{ holiday.bookingDetails.lengthOfStay} { holiday.bookingDetails.lengthOfStay > 1 ? 'days' : 'day' }</strong>
+                &nbsp;
+              </span>
+              <br />
+              <span>
+                Departing from <strong>{ holiday.flightDetails.departureAirport }</strong>
+                &nbsp;
+              </span>
+            </p>
+            <a className={classNames(styles.holidayCardButton, 'button')} href="#">
+              <span>Book now</span>
+              <br />
+              <span className={styles.holidayCardButtonLargeText}>£{holiday.bookingDetails.price.amount.toFixed(2)}</span>
+            </a>
           </div>
         </div>
         <section className={styles.holidayCardDetail}>
